603b4d9f7cca99f5823a8e8312f2c19ea17b08ec
Mohit-inco <incognito0bc@gmail.com>
2025-03-30 22:29:45 +0530

    Update ImageGrid

--- src/components/ImageGrid.tsx
+++ src/components/ImageGrid.tsx
@@ -46,17 +46,17 @@ const ImageGrid: React.FC<ImageGridProps> = ({ images, columns = 3 }) => {
   
   return (
     <>
-      <div className={`grid ${gridClass} gap-4 md:gap-6`}>
+      <div className={`grid ${gridClass} gap-3 md:gap-4`}>
         {images.map((image) => (
           <div key={image.id} className="group rounded-md overflow-hidden">
             <div 
-              className="hover-image-card bg-muted relative cursor-pointer"
+              className="hover-image-card aspect-[4/3]  bg-muted relative cursor-pointer"
               onClick={() => openModal(image)}
             >
               <img 
                 src={image.src} 
                 alt={image.alt} 
-                className="w-full h-auto object-cover"
+                className="w-full h-full object-cover"
                 loading="lazy"
               />
               <div className="image-overlay">
